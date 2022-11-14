0a03cc6dd9787727ec0fbbc8ac7abf353a78f231
emili <emilie.guilhot@uha.fr>
2022-11-14 11:45:37 +0100

    master

--- client1.py
+++ client1.py
@@ -1,8 +1,9 @@
 import socket
 
 client_socket = socket.socket()
-client_socket.connect((host, port))
+client_socket.connect(('127.0.0.1', 10000))
+message = "bonsoir"
 client_socket.send(message.encode())
 data = client_socket.recv(1024).decode()
-print("bonsoir")
+print("Message reçu:",data)
 client_socket.close()
--- serveur1.py
+++ serveur1.py
@@ -2,10 +2,11 @@ import socket
 
 host = socket.gethostname()
 server_socket = socket.socket()
-server_socket.bind((host, 10000))
+server_socket.bind(('127.0.0.1', 10000))
 server_socket.listen(1)
 conn, address = server_socket.accept()
 data = conn.recv(1024).decode()
-print("bonjour")
+print ("Message reçu:",data)
+reply = "bonjour"
 conn.send(reply.encode())
 conn.close()
