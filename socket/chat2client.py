181acba73c495dd6af73669ef15bea0dd84895f9
emili <emilie.guilhot@uha.fr>
2022-11-15 11:51:11 +0100

    master

---
+++ socket/chat2client.py
@@ -0,0 +1,12 @@
+import socket
+
+client_socket = socket.socket()
+client_socket.connect(('127.0.0.1', 10000))
+msg = ""
+msgserv = ""
+while msg !="bye" and msgserv !="bye" and msg !="arret" and msgserv !="arret" :
+    msg = input("Entrez votre message:")
+    client_socket.send(msg.encode())
+    msgserv = client_socket.recv(1024).decode()
+    print("Message reçu:",msgserv)
+client_socket.close()
